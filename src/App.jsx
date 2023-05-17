0ddfdf42fef8754913ab814b5626eea3b5d17307
Serajur-Reza <rezainfinity54@gmail.com>
2023-05-17 23:25:10 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,159 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+} from "@mui/material";
+
+function App() {
+  const [data, setData] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [editId, setEditId] = useState(null);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setData(res.data))
+      .catch((err) => setData(null));
+  }, []);
+
+  console.log(data);
+
+  const handleEdit = (id, item) => {
+    setEditId(id);
+    setOpen(true);
+    setFormData({
+      name: item.name,
+      username: item.username,
+      email: item.email,
+    });
+    console.log("edited", id, item);
+  };
+
+  const handleForm = (e) => {
+    console.log(e.target.value);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleDelete = (id) => {
+    const tempData = data.filter((item) => item.id !== id);
+    console.log(tempData);
+    setData(tempData);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setEditId(null);
+  };
+
+  const handleSave = () => {
+    //let index = 0
+    const index = data.findIndex((item) => item.id === editId);
+    const tempData = data;
+    tempData[index].name = formData.name;
+    tempData[index].username = formData.username;
+    tempData[index].email = formData.email;
+    // tempData.push(formData);
+    setData(tempData);
+    setOpen(false);
+    setEditId(null);
+  };
+  return (
+    <>
+      <h1>Hello WOrld</h1>
+      <table>
+        <tr>
+          <th>name</th>
+          <th>username</th>
+          <th>email</th>
+        </tr>
+
+        {data && data?.length ? (
+          data?.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td>
+                <Button
+                  variant="outlined"
+                  onClick={() => handleEdit(item.id, item)}
+                >
+                  Edit
+                </Button>
+              </td>
+              <td>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <p>no data available</p>
+        )}
+      </table>
+
+      <Dialog
+        open={open}
+        //onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Edit user info?</DialogTitle>
+        <DialogContent>
+          {/* <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText> */}
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            placeholder="name"
+            onChange={handleForm}
+          />
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            placeholder="username"
+            onChange={handleForm}
+          />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            placeholder="email"
+            onChange={handleForm}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+          <Button onClick={handleSave} autoFocus>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+export default App;
